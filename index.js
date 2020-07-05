d88abedf5917f817e6485642d011006be741ed35
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-05 21:40:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function addToCart(item) {
     itemPrice : numInternal,
   }
   cart.push(objInternal);
-
+  console.log("------------CONSOLE LOG IS HERE");
   return `${item} has been added to your cart.`
 
 }

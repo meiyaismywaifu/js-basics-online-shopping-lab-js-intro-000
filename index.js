3944b3977206388001eb419b9c7dfd5829549b58
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-05 22:01:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ function viewCart() {
 
 function total() {
   var numInternal = 0;
+  
   for (var i = 0; i < cart.length; i++){ // AAAAAAAAAAAAAAAAAA
     numInternal = numInternal + parseInt(cart[i].itemPrice);
       // oh yeah can do sequential recall horizontally too. forgot.
